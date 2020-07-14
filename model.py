d72e05bf82447ce116221cb2f615ed4470fd85fa
pralayesh <67859458+pralayesh@users.noreply.github.com>
2020-07-14 23:33:11 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,64 +1,70 @@
 import numpy as np
-#import matplotlib.pyplot as plt
 import pandas as pd
-import keras
-from keras.models import Sequential,model_from_json
-from keras.layers import Dense,LSTM,Dropout,Activation
-from keras.layers.embeddings import Embedding
-import os
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import MultinomialNB
+import random
+import re
 import pickle
-from keras.datasets import imdb
-from keras.preprocessing.sequence import pad_sequences
 
-max_words = 20000
-(X_train, y_train), (X_test, y_test) = imdb.load_data(seed=1, num_words=max_words)
+data = pd.read_csv('desktop/IMDB_Dataset.csv')
 
-max_review_len=80
-X_train=pad_sequences(X_train,truncating='pre', padding='pre', maxlen=max_review_len)
-X_test=pad_sequences(X_test,truncating='pre', padding='pre', maxlen=max_review_len)
-
-embed_vector_len=32
-
-model=Sequential()
-model.add(Embedding(input_dim=max_words,output_dim=embed_vector_len,embeddings_initializer='uniform',mask_zero=True))
-model.add(LSTM(100,kernel_initializer='uniform',dropout=0.2,recurrent_dropout=0.2))
-model.add(Dense(1,activation='sigmoid'))
-
-model.compile(optimizer='Adam',loss='binary_crossentropy',metrics=['accuracy'])
-model.summary()
-
-History=model.fit(X_train,y_train,shuffle=True,batch_size=128,epochs=5)
-
-model_json = model.to_json()
-with open('model.json','w') as json_file:
-  json_file.write(model_json)
-model.save_weights("model.h5")
-
-json_file = open('model.json','r')
-loaded_model_json = json_file.read()
-json_file.close()
-
-loaded_model = model_from_json(loaded_model_json)
-loaded_model.load_weights("model.h5")
-
-d=imdb.get_word_index()
-review=input('Enter your review:').lower()
-words = review.split()
-review = []
-for word in words:
-  if word in d:
-    if d[word] > 20000: 
-      review.append(2)
+print(data.shape)
+data=data.dropna()
+data=data.sample(frac=1).reset_index(drop=True)
+data=data.iloc[:12000,:]
+X=data['review']
+Y=data['sentiment']
+y=[]
+for i in range(len(Y)):
+    if(Y[i]=='positive'):
+        y.append(1)
     else:
-      review.append(d[word]+3)
-  else:
-    review.append(2)
+        y.append(0)
+        
+print(y[:10])
+
+ps=PorterStemmer()
+corpus=[]
+for i in range(0,len(data)):
+    review = re.sub('[^a-zA-Z]',' ',data.loc[i,['review']].values[0])
+    review = review.lower()
+    review = review.split()
     
-review=pad_sequences([review],truncating='pre', padding='pre', maxlen=max_review_len)
-print(review)
-prediction=loaded_model.predict(review)
-print(prediction)
-if(prediction[0][0]<0.5):
-  print('Negetive')
-else:
-  print('Positive')
+    review=[ps.stem(word) for word in review if not word in stopwords.words('english')]
+    review=' '.join(review)
+    corpus.append(review)
+
+review=list(corpus[:-8])
+sentiment=list(y)
+print(len(review))
+print(len(sentiment))
+dic={'review':review,'sentiment':y}
+df=pd.DataFrame(dic)
+df.to_csv('review_sentiment_data.csv')
+
+tv = TfidfVectorizer(max_features=5000,ngram_range=(1,2))
+X=tv.fit_transform(X)
+
+y=np.array(y)
+X_train,X_test,y_train,y_test = train_test_split(X,y,random_state=1)
+print(X_train.shape)
+print(X_test.shape)
+print(y_train.shape)
+
+clf = MultinomialNB(alpha=6)
+clf.fit(X_train,y_train)
+
+y_pred=clf.predict(X_test)
+
+print(accuracy_score(y_test,y_pred))
+
+filename = 'sentiment_model.sav'
+pickle.dump(clf, open(filename, 'wb'))
+ 
+loaded_model = pickle.load(open(filename, 'rb'))
+
+print(loaded_model.score(X_test,y_test))
